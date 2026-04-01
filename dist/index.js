f71784899f60947fe8c52800193ec2a427534174
genericdelta <repo4maintenance@gmail.com>
2026-04-01 13:13:01 +0800

    Removed SocketProvider as its context complicates with Next.js and react instance

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,4 @@
 // extensions included because of nodenext and esnext in tsconfig.json
 export * from './types/room.types.js';
 export * from './hooks/useChat.js';
-export * from './providers/SocketProvider.js';
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 // extensions included because of nodenext and esnext in tsconfig.json
 export * from './types/room.types.js';
 export * from './hooks/useChat.js';
-export * from './providers/SocketProvider.js';
