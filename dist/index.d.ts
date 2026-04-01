87c8902a6fb0b7fcaf88c2305b622e16bab190d1
genericdelta <repo4maintenance@gmail.com>
2026-04-01 13:19:37 +0800

    Exported provider types

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
 export * from './types/room.types.js';
 export * from './hooks/useChat.js';
+export * from './types/provider.types.js';
 //# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,5 @@
 // extensions included because of nodenext and esnext in tsconfig.json
 export * from './types/room.types.js';
 export * from './hooks/useChat.js';
+export * from './types/provider.types.js';
 //# sourceMappingURL=index.js.map
